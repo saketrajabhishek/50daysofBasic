4715342a2e381da4dd4c8cc6e6c8001c624c1f48
saketrajabhishek <saketrajabhishek@gmail.com>
2022-08-22 02:16:55 +0530

    Feat: Exapnding Cards Done

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,1 +1,13 @@
-console.log(`Expanding Cards`);
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
